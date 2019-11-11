c5a12719193f21fa2a6215d9e136dcab1991a9ec
brandonrvice@gmail.com <38141231+brandonrvice@users.noreply.github.com>
2019-11-10 22:18:00 -0800

    first

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+import articleData from "./article-data.json";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/api/articles/:id/upvote", (req, res) => {
+  const article = articleData.find(p => p.id === req.params.id);
+  article.upVote = article.upVote === undefined ? 1 : ++article.upVote;
+  res.status(200).send({
+    id: article.id,
+    upVote: article.upVote
+  });
+});
+
+app.get("/api/article/:id", (req, res) => {
+  let article = articleData.find(p => p.id === req.params.id);
+  res.status(200).send(article);
+});
+
+app.get("/api/articles", (req, res) => {
+  res.status(200).send(articleData);
+});
+
+app.listen(8000, () => console.log("Listening on port 8000."));
