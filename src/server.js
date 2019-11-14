745fe93fb4721c1c5018374a16c5f562406c9c2c
brandonrvice@gmail.com <38141231+brandonrvice@users.noreply.github.com>
2019-11-13 20:19:35 -0800

    Put listening port into .env file.

--- src/server.js
+++ src/server.js
@@ -10,11 +10,10 @@ import routes from "./routes";
 dotenv.config();
 
 const app = express();
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), { flags: "a" });
+
 app.use(cors());
 app.use(bodyParser.json());
-
-var accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), { flags: "a" });
 app.use(morgan("combined", { stream: accessLogStream }));
 app.use("/api", routes);
-
-app.listen(8000, () => console.log("Listening on port 8000."));
+app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}.`));
